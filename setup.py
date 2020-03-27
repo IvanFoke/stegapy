b5243f11f8e706c034efb1586b807e926ab2def7
IvanFoke <58694429+IvanFoke@users.noreply.github.com>
2020-03-27 15:22:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A simple steganography project with Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    # url="https://github.com/pypa/sampleproject",
+    url="https://github.com/IvanFoke/stegapy",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
